dc19f3a00c7b05d35594dfb3664e665ec93f1bba
DPS0340 <jh001007@gmail.com>
2018-12-30 20:37:20 +0900

    init

---
+++ Lotto.js
@@ -0,0 +1,32 @@
+const getone = (arr, every_number) => {
+    arr.push(Math.floor(Math.random() * every_number) + 1);
+};
+const get = (arr, choice_number, every_number) => {
+    for(let i=0; i<choice_number;i++) {
+        getone(arr, every_number);
+    }
+};
+const answercheck = (answer, mychoice) => {
+    let flag = false;
+    for(let i=0; i<answer.length;i++) {
+        if(answer[i] != mychoice[i]) flag = false;
+    }
+    return flag;
+};
+const auto_roll = (choice_number, every_number) => {
+    answer = [];
+    mychoice = [];
+    get(answer, choice_number, every_number);
+    get(mychoice, choice_number, every_number);
+    console.log(answer);
+    console.log(mychoice);
+    return answercheck(answer, mychoice);
+};
+const manual_roll = (mychoice, choice_number, every_number) => {
+    answer = [];
+    get(answer, choice_number, every_number);
+    console.log(answer);
+    console.log(mychoice);
+    return answercheck(answer, mychoice);
+};
+console.log(auto_roll(8, 45));
