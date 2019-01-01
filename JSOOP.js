e73a019e4644a28c1bb07595b5b99a0ec5106caa
DPS0340 <jh001007@gmail.com>
2019-01-01 19:14:05 +0900

    Dice Roll

--- JSOOP.js
+++ JSOOP.js
@@ -2,17 +2,26 @@ function Dice(k) {
     this.k = k;
     console.log(`${k}면체 주사위`);
 }
-Dice.prototype.roll = (n) => {
+Dice.prototype.roll = function (n) {
     let results = [];
-    let sum = 0;
     for(let i=0;i<n;i++) {
         let result = Math.floor(Math.random() * this.k) + 1;
         results.push(result);
-        sum += result;
     }
-    console.log(results);
-    return sum;
+    return results;
+};
+Dice.prototype.get = function () {
+    return this.k;
 };
 
-dice = new Dice(20);
-console.log(dice.roll(5));
+function play(bet, k, n) {
+    let dice = new Dice(k);
+    let result = dice.roll(n);
+    if (result > k * n / 2) {
+        return bet * 2;
+    } else if (result < k * n / 2) {
+        return 0;
+    } else {
+        return bet;
+    }
+}
